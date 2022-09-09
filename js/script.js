74260a9392cae72494b071b3b0816cf032f66c3a
Birendra Jung Rai <bjsampang@gmail.com>
2022-09-09 11:38:02 +0545

    transferred to xampp

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 const navSlide = ()=> {
     const burger = document.querySelector('.burger') ;
     const nav = document.querySelector('.nav-links') ;
-    const navlinks = document.querySelectorAll('nav-links li')
+    const navlinks = document.querySelectorAll('.nav-links li')
 
     burger.addEventListener('click',()=>{
         nav.classList.toggle('nav-active') ;
-    })
+    
+
+    // animate burger
+    burger.classList.toggle('toggle') ;
+   
+
+    }) ;
+   
+
+   
+    
 }
+
 
 navSlide();
